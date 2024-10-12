e28f5dd2f05f221cfd0cd5b49439243dfcb48d12
Robit11618 <160984230+Robit11618@users.noreply.github.com>
2024-10-12 10:51:45 -0400

    auto started

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GremlinsAutoBR.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Autonomous
+public class GremlinsAutoBR extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+
+        SampleMecanumDrive drivetrain = new SampleMecanumDrive(hardwareMap);
+
+        waitForStart();
+
+          /*  Trajectory goForward = drivetrain.trajectoryBuilder(new Pose2d(0, 0, 0))
+                    .forward(100)
+                    .build();
+            drivetrain.followTrajectory(goForward);
+
+            Trajectory lineToPosition = drivetrain.trajectoryBuilder(new Pose2d(10, 0, 0))
+                    .lineTo(new Vector2d(0, 0))
+                    .build();
+            drivetrain.followTrajectory(lineToPosition);
+
+
+            Trajectory strafeLeft = drivetrain.trajectoryBuilder(new Pose2d(0, 0, 0))
+                    .strafeLeft(50)
+                    .build();
+            drivetrain.followTrajectory(strafeLeft);
+
+            Trajectory strafeToPosition = drivetrain.trajectoryBuilder(new Pose2d(0, 0))
+                    .strafeTo(new Vector2d(0, 50))
+                    .build();
+            drivetrain.followTrajectory(strafeToPosition);*/
+
+        //actual auto
+        Trajectory strafeLeft = drivetrain.trajectoryBuilder(new Pose2d(0, 0, 0))
+                .strafeLeft(22)
+                .build();
+        drivetrain.followTrajectory(strafeLeft);
+
+        Trajectory strafeRight = drivetrain.trajectoryBuilder(new Pose2d(0, 0, 0))
+                .strafeRight(2)
+                .build();
+        drivetrain.followTrajectory(strafeRight);
+
+        Trajectory goForward = drivetrain.trajectoryBuilder(new Pose2d(0, 0, 0))
+                .forward(23)
+                .build();
+        drivetrain.followTrajectory(goForward);
+
+        drivetrain.trajectoryBuilder(new Pose2d(0, 0, 0))
+                .strafeRight(104)
+                .build();
+        drivetrain.followTrajectory(strafeRight);
+
+        Trajectory back = drivetrain.trajectoryBuilder(new Pose2d(0, 0, 0))
+                .back(17)
+                .build();
+        drivetrain.followTrajectory(back);
+
+
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GremlinsFIRSTOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GremlinsFIRSTOpmode.java
@@ -13,36 +13,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
-@Autonomous
-    public class GremlinsFIRSTOpmode extends LinearOpMode {
-        @Override
-        public void runOpMode() {
-            SampleMecanumDrive drivetrain = new SampleMecanumDrive(hardwareMap);
 
-            waitForStart();
-
-            Trajectory goForward = drivetrain.trajectoryBuilder(new Pose2d(0, 0, 0))
-                    .forward(100)
-                    .build();
-            drivetrain.followTrajectory(goForward);
-
-            Trajectory lineToPosition = drivetrain.trajectoryBuilder(new Pose2d(10, 0, 0))
-                    .lineTo(new Vector2d(0, 0))
-                    .build();
-            drivetrain.followTrajectory(lineToPosition);
-
-
-            Trajectory strafeLeft = drivetrain.trajectoryBuilder(new Pose2d(0, 0, 0))
-                    .strafeLeft(50)
-                    .build();
-            drivetrain.followTrajectory(strafeLeft);
-
-            Trajectory strafeToPosition = drivetrain.trajectoryBuilder(new Pose2d(0, 0))
-                    .strafeTo(new Vector2d(0, 50))
-                    .build();
-            drivetrain.followTrajectory(strafeToPosition);
-        }
-    }
 
 
 

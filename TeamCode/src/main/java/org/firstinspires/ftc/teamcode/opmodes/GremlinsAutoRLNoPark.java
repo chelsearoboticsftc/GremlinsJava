ef8f4410aeb5fb1f65b499826df73903a34e9c19
Robit11618 <160984230+Robit11618@users.noreply.github.com>
2024-11-11 18:20:07 -0500

    Update GremlinsAutoRLNoPark.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GremlinsAutoRLNoPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GremlinsAutoRLNoPark.java
@@ -1,4 +1,50 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-public class GremlinsAutoRLNoPark {
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Autonomous
+public class GremlinsAutoRLNoPark extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+
+        SampleMecanumDrive drivetrain = new SampleMecanumDrive(hardwareMap);
+
+        waitForStart();
+
+        Pose2d startPose = new Pose2d(-39.5,-62,Math.toRadians(90));
+
+        drivetrain.setPoseEstimate(startPose);
+
+
+        Trajectory traj1 = drivetrain.trajectoryBuilder(startPose)
+                //Push Sample
+                .strafeLeft(22)
+                .build();
+
+        Trajectory traj2 = drivetrain.trajectoryBuilder(traj1.end())
+                .strafeRight(12)
+                .build();
+
+        Trajectory traj3 = drivetrain.trajectoryBuilder(traj2.end())
+                //Move Away From Wall
+                .forward(12)
+                .build();
+
+
+        drivetrain.followTrajectory(traj1);
+        drivetrain.followTrajectory(traj2);
+        drivetrain.followTrajectory(traj3);
+
+
+    }
+
 }
+
+

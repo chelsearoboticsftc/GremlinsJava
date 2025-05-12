55baf570f9eed9a526c2c16a5abd23f3edd9a4e9
Robit11618 <160984230+Robit11618@users.noreply.github.com>
2025-05-12 18:34:21 -0400

    Cleans Up Java files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testLS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testLS.java
@@ -1,8 +1,5 @@
 //package org.firstinspires.ftc.teamcode.opmodes;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 //import org.firstinspires.ftc.teamcode.subsystems.LS;
 
 /*@TeleOp

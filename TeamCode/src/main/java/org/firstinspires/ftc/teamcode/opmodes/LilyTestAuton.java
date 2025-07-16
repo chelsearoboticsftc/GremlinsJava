7b6b13086ce919eb5a5241c21b948fca9a6926e7
Robit11618 <160984230+Robit11618@users.noreply.github.com>
2025-07-16 19:58:43 -0400

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LilyTestAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LilyTestAuton.java
@@ -2,24 +2,29 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.deliveryConstants;
+import org.firstinspires.ftc.teamcode.subsystems.deliverySUbsystem;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 @Autonomous
-public class LilyTestAuton {
+public class LilyTestAuton extends LinearOpMode{
+enum State{
 
-    @Override
+    IDLE,
+    STRAFE_RIGHT,
+    FOWARD,
+    STRAFE_LEFT,
+    BACK
+}
+State currentState = State.IDLE;
+
+@Override
     public void runOpMode(){
-        SampleMecanumDrive drivetrain = new SampleMecanumDrive(hardwareMap);
 
-        waitForStart();
-
-        Pose2d startPose = new Pose2d(x: 10, y: -5);
-
-        drivetrain,setPoseEstimate(startPose);
-    }
+}
 }
